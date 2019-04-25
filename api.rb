a4a0b2bcd6cd6c51723ffab5e881c85f3ac37f1f
Anibal Linares <anibal.linares.19@gmail.com>
2019-04-24 20:18:29 -0400

    Prueba Final se agrega github

---
+++ api.rb
@@ -0,0 +1,42 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
+def request(address,key)
+
+  url = URI(address+key)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  request = Net::HTTP::Get.new(url)
+  request["cache-control"] = 'no-cache'
+  request["postman-token"] = 'c1144cce-d324-d879-0889-c3947ad163d2'
+  response = http.request(request)
+  JSON response.read_body
+
+end
+address ="https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000"
+key="&api_key=M1REUtc9ZqdjuJAOZVK1tfwpJeYzHkd8URtVUN0H"
+
+connection = request(address,key)
+connection = connection["photos"]
+
+photos = connection.map{|x| x['img_src']}
+
+def buid_web_page(photos)
+html= " <html>
+  <head>
+  </head>
+  <body>
+  <ul>"
+  photos.each do |photo|
+  html += "<img src=\"#{photo}\">\n"
+  end
+  html+="</ul>
+  </body>
+  </html>"
+
+  File.write('output.html', html)
+end
+
+buid_web_page(photos)
